869860885b20363cc00e4d9af20a494a5e9be2b4
Хасп Элд <EldHasp@Gmail.com>
2021-03-17 23:08:28 +0300

    Восстановление репозитория

---
+++ SatanPassword.Sln/SatanPasswordGetValue/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SatanPasswordGetValue
+{
+    class Program
+    {
+        private static string fileBin = "pi.bin";
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                fileBin = args[0];
+
+            KeyToValue(fileBin);
+
+        }
+
+        static void KeyToValue(string fileBin)
+        {
+            uint[] values;
+            using (var file = File.OpenRead(fileBin))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                values = (uint[])formatter.Deserialize(file);
+            }
+
+            bool isCancel = false;
+            int key;
+            while (!isCancel)
+            {
+                while (!TryIntInput("Введите ключ - восемь цифр второй части пароля (Пустая строка -- Cancel): ",
+                    out key, out isCancel))
+                {
+                    Console.WriteLine("Ошибка ввода! Повторите!");
+                }
+                if (!isCancel)
+                {
+                    uint value = values[key];
+                    if (value == 0xFF_FF_FF_FF)
+                        Console.WriteLine("Нет значения для этого кляюча.");
+                    else
+                        Console.WriteLine($"Первая часть пароля {Convert.ToString(value, 16)}.");
+                }
+            }
+
+
+        }
+
+        static readonly uint keyMax = (uint)Math.Pow(10, 8);
+        static bool TryIntInput(string message, out int result, out bool cancel)
+        {
+            result = default;
+            cancel = default;
+            Console.Write(message);
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                cancel = true;
+                return true;
+            }
+            if (uint.TryParse(input, out uint num) && num < keyMax)
+            {
+                result = (int) num;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SatanPassword.Sln/SatanPasswordTxtToValuesConverter/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SatanPasswordTxtToValuesConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileTxt = "pi.txt";
+            string fileBin = "pi.bin";
+            string fileNotKey = "not-key.txt";
+
+            if (args.Length > 0)
+                fileTxt = args[0];
+            if (args.Length > 1)
+                fileBin = args[1];
+            if (args.Length > 2)
+                fileNotKey = args[2];
+
+            TxtToValuesConvert(fileTxt, fileBin, fileNotKey);
+
+        }
+
+      static  readonly int length = (int)Math.Pow(10, 8);
+        static void TxtToValuesConvert(string fileTxt, string fileBin, string fileNotKey)
+        {
+            uint value = 0, key = 0;
+            int letter;
+            uint[] values = Enumerable.Repeat(0xFF_FF_FF_FF, length).ToArray();
+            uint[] counts = new uint[length];
+            uint countsMax = 0;
+            int countsMaxKey = 0;
+
+            var file = File.OpenRead(fileBin);
+
+            // Заполнение начального значения
+            for (int i = 0; i < 7; i++)
+            {
+                letter = file.ReadByte();
+                value |= TryDigitMessagePosition(letter) << (7 - i) * 4;
+            }
+
+            // Заполнение начального ключа
+            for (int i = 0; i < 8; i++)
+            {
+                letter = file.ReadByte();
+                key |= TryDigitMessagePosition(letter) << (8 - i) * 4;
+            }
+
+            //Посимвольное считывание
+            while ((letter = file.ReadByte()) >= 0)
+            {
+                // Получение новых значения и ключа.
+                value <<= 4;
+                value |= key >> 28;
+                key <<= 4;
+                key |= TryDigitMessagePosition(letter);
+                uint index = HexDecimalToUInt(key);
+
+                // Проверка был ли уже такой ключ
+                if (counts[index]==0)
+                    values[index] = value;
+
+                counts[index]++;
+
+            }
+
+            uint TryDigitMessagePosition(int digit)
+            {
+                if (TryDigit(digit, out uint result))
+                    return result;
+                throw new Exception($"В позиции {file.Position} не цифра.");
+            }
+        }
+
+        static bool TryDigit(int digit, out uint result)
+        {
+            digit -= 0x30;
+            if (digit < 0 || digit > 9)
+            {
+                result = default;
+                return false;
+            }
+            result = (uint)digit;
+            return true;
+        }
+
+        static uint HexDecimalToUInt(uint hexDecimal)
+        {
+            uint result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result *= 10;
+                result += hexDecimal >> 28;
+                hexDecimal <<= 4;
+            }
+            return result;
+        }
+    }
+}
